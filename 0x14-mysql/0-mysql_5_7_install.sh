6415bc65c3fae1f520434a3fceb774d92f4ad9b7
tafe124 <tafesegechera22@gmail.com>
2023-02-04 01:03:05 +0000

    done

---
+++ 0x14-mysql/0-mysql_5_7_install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Install myqls version 5.7
+echo 'deb http://repo.mysql.com/apt/ubuntu/ trusty mysql-5.7-dmr' | sudo tee -a /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get install mysql-server-5.7
+mysql --version
